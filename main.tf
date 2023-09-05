42a1ebbfe379510a14966ebebd1f44909fa28dfc
Gandobil <ab.malek11@gmail.com>
2023-09-05 17:12:38 -0400

    resource "aws_instance" "app_server2" {   ami           = var.ami   instance_type = var.instance_type   vpc_security_group_ids = var.vpc_security_group_ids   key_name = "Test"
      tags = {
        Name = var.name
        Owner = var.owner
        Product = var.product
      }
    }

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "app_server2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  key_name = "Test"
+
+  tags = {
+    Name = var.name
+    Owner = var.owner
+    Product = var.product
+  }
+}
